1c0715a79a06f81c04b2dfc96a546c8f1c47b08b
Boyd Hogerheijde <boydhogerheijde@gmail.com>
2017-03-02 12:21:05 +0100

    Edited model to conform to feedback from Floris.

--- src/main/kotlin/com/tsse/model/Models.kt
+++ src/main/kotlin/com/tsse/model/Models.kt
@@ -3,9 +3,16 @@ package com.tsse.model
 import java.util.*
 
 /**
+ * File containing all models contained in the database.
+ *
+ * The unit of {@link ExerciseInfo.weight} is in kilograms.
+ *
+ * @author Fabian de Almeida Ramos
  * @author Boyd Hogerheijde
+ *
+ * @version 1.0.0
  */
-data class User(val id: Long, val username: String, val password: String)
+data class User(val id: Long, val username: String, val email: String, val password: String)
 
 data class Schedule(val id: Long, val name: String,
                     val description: String, val workouts: List<Workout>,
@@ -15,7 +22,7 @@ data class Workout(val id: Long, val name: String, val description: String, val 
 
 data class Exercise(val id: Long, val name: String,
                     val description: String, val category: Category,
-                    val mediaFiles: List<String>, var exerciseInfo: ExerciseInfo)
+                    val mediaFiles: List<String>)
 
 data class ExerciseInfo(val id: Long, val exercise: Exercise, val sets: Int, val reps: Int, val weight: Double)
 
@@ -24,5 +31,3 @@ data class PersonalExercise(val user: User, val workout: Workout, val exerciseIn
 data class PersonalSchedule(val user: User, val schedule: Schedule, val startDate: Date, val endDate: Date)
 
 enum class Category
-
-
