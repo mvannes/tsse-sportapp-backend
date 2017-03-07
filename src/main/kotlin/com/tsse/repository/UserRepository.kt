613820cdfcc04a2c75c1b5a43954f87fcbb16580
Fabian <sadipo34@gmail.com>
2017-03-07 15:24:36 +0100

    TSSE64-Created user repository.

---
+++ src/main/kotlin/com/tsse/repository/UserRepository.kt
@@ -0,0 +1,15 @@
+package com.tsse.repository
+
+import com.tsse.model.User
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+
+/**
+ * Repository for users.
+ *
+ * @author Fabian de Almeida Ramos
+ *
+ * @version 1.0.0
+ */
+@RepositoryRestResource
+interface UserRepository : CrudRepository<User, Long>
