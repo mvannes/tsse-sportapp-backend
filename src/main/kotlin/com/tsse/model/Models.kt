dd2dd974c6d8fbf34356628f5bc2cf8b7b474c46
Fabian <sadipo34@gmail.com>
2017-03-07 15:43:02 +0100

    Changed tag ElementCollection to OneToMany for workout list.
    Changed it to OneToMany; one schedule can have many workouts.

--- src/main/kotlin/com/tsse/model/Models.kt
+++ src/main/kotlin/com/tsse/model/Models.kt
@@ -17,7 +17,7 @@ data class User(val id: Long, val username: String, val email: String, val passw
 
 @Entity
 data class Schedule(@Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long, val name: String,
-                    val description: String, @ElementCollection (targetClass = Workout::class) val workouts: List<Workout>,
+                    val description: String, @OneToMany(mappedBy = "schedule") val workouts: List<Workout>,
                     val amountOfTrainingsPerWeek: Int)
 
 data class Workout(val id: Long, val name: String, val description: String, val exercises: List<Exercise>)
