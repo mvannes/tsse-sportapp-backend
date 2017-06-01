51fa1ad945a126b6e4aa9f6ab3797fe3f91ca67f
Boyd Hogerheijde <boydhogerheijde@gmail.com>
2017-06-01 22:11:49 +0200

    fixed controller tests, @Ignored broken tests for the time being.

--- src/test/kotlin/com/tsse/controller/ScheduleControllerTests.kt
+++ src/test/kotlin/com/tsse/controller/ScheduleControllerTests.kt
@@ -1,7 +1,27 @@
 package com.tsse.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.tsse.domain.model.Schedule
+import com.tsse.service.ScheduleService
+import org.hamcrest.Matchers.equalTo
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.BDDMockito.given
+import org.mockito.BDDMockito.willDoNothing
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.springframework.data.rest.webmvc.ResourceNotFoundException
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 /**
  * Tests related to the ScheduleController.
@@ -13,183 +33,187 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(SpringRunner::class)
 class ScheduleControllerTests {
 
-//    @Mock lateinit var service: ScheduleService
-//
-//    @InjectMocks lateinit var scheduleController: ScheduleController
-//
-//    lateinit var mockMvc: MockMvc
-//
-//    val URI = "/api/schedule/"
-//
-//    @Before
-//    fun setup() {
-//        MockitoAnnotations.initMocks(ScheduleControllerTests::class)
-//        mockMvc = MockMvcBuilders
-//                .standaloneSetup(scheduleController)
-//                .build()
-//    }
-//
-//    @Test
-//    fun testSaveSchedule_returnsHttpStatusIsCreated() {
-//
-//        val schedule = Schedule("Name", "Description", ArrayList(), 1)
-//
-//        given(service.saveSchedule(schedule)).willReturn(schedule)
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.post(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(schedule)))
-//                .andExpect(status().isCreated)
-//                .andExpect { content().equals(schedule) }
-//    }
-//
-//    @Test
-//    @Ignore
-//    fun testSaveScheduleEmptyName_returnsHttpStatusIsBadRequest() {
-//
-//        val schedule = Schedule("", "Description", ArrayList(), 1)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.post(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(schedule)))
-//                .andExpect(status().isBadRequest)
-//                .andExpect { jsonPath("message", equalTo("Object sent is not valid: [Schedule name cannot be empty!]")) }
-//    }
-//
-//    @Test
-//    fun testSaveScheduleEmptyDescription_returnsHttpStatusIsCreated() {
-//
-//        val schedule = Schedule("Name", "", ArrayList(), 1)
-//
-//        given(service.saveSchedule(schedule)).willReturn(schedule)
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.post(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(schedule)))
-//                .andExpect(status().isCreated)
-//                .andExpect { content().equals(schedule) }
-//    }
-//
-//    @Test
-//    @Ignore
-//    fun testSaveScheduleEmptyAmountOfTrainings_returnsHttpStatusIsBadRequest() {
-//
-//        val schedule = Schedule("Name", "Description", ArrayList(), 0)
-//
-//        given(service.saveSchedule(schedule)).willReturn(schedule)
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.post(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(schedule)))
-//                .andExpect(status().isBadRequest)
-//                .andExpect { jsonPath("message", equalTo("Object sent is not valid: [Schedule needs at least one training per week!]")) }
-//    }
-//
-//    @Test
-//    fun testGetScheduleById_returnsHttpStatusOk() {
-//
-//        val schedule = Schedule("Name", "Description", ArrayList(), 0)
-//        val id = 1L
-//        given(service.getSchedule(id)).willReturn(schedule)
-//
-//        mockMvc.perform(
-//                get(URI + "{id}", id))
-//                .andExpect(status().isOk)
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-//                .andExpect { content().equals(schedule) }
-//
-//
-//    }
-//
-//    @Test
-//    fun testGetScheduleById_returnsHttpStatusIsNotFound() {
-//
-//        val id = 1L
-//        given(service.getSchedule(id)).willThrow(ScheduleNotFoundException::class.java)
-//
-//        mockMvc.perform(
-//                get(URI + "{id}", id))
-//                .andExpect(status().isNotFound)
-//    }
-//
-//    @Test
-//    fun testGetAllSchedules_returnsHttpStatusOk() {
-//
-//        val results = arrayListOf(Schedule("Name", "Description", ArrayList(), 0),
-//                Schedule("Name2", "Description2", ArrayList(), 0))
-//
-//
-//        given(service.getAllSchedules()).willReturn(results)
-//
-//        mockMvc.perform(
-//                get(URI))
-//                .andExpect(status().isOk)
-//                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-//                .andExpect { content().equals(results) }
-//
-//    }
-//
-//    @Test
-//    fun testUpdateSchedule_returnsHttpStatusOk() {
-//        val schedule = Schedule("Name", "Description", ArrayList(), 1)
-//
-//        given(service.updateSchedule(schedule)).willReturn(schedule)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.put(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(schedule)))
-//                .andExpect(status().isOk)
-//                .andExpect { content().equals(schedule) }
-//
-//    }
-//
-//
-//    @Test
-//    @Ignore
-//    fun testUpdateScheduleInvalidForm_returnsHttpStatusBadRequest() {
-//
-//        val schedule = Schedule("", "Description", ArrayList(), 1)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.put(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(schedule)))
-//                .andExpect(status().isBadRequest)
-//                .andExpect { jsonPath("message", equalTo("Object sent is not valid: [Schedule name cannot be empty!]")) }
-//
-//    }
-//
-//    @Test
-//    fun testUpdateScheduleNonExistingSchedule_returnsHttpStatusNotFound() {
-//
-//        val schedule = Schedule("Name", "Description", ArrayList(), 1)
-//
-//        given(service.updateSchedule(schedule)).willThrow(ScheduleNotFoundException::class.java)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.put(URI)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(asJsonString(schedule)))
-//                .andExpect(status().isNotFound)
-//
-//    }
-//
-//    @Test
-//    fun testDeleteSchedule_returnsHttpStatusNoContent() {
-//
-//        val id = 1L
-//
-//        willDoNothing().given(service).deleteSchedule(id)
-//
-//        mockMvc.perform(
-//                delete(URI + "{id}", id))
-//                .andExpect(status().isNoContent)
-//                .andExpect (status().isNoContent)
-//    }
-//
-//    private fun asJsonString(obj: Any): String = ObjectMapper().writeValueAsString(obj)
-//
+    @Mock lateinit var service: ScheduleService
+
+    @InjectMocks lateinit var scheduleController: ScheduleController
+
+    lateinit var mockMvc: MockMvc
+
+    val URI = "/api/schedule/"
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(ScheduleControllerTests::class)
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(scheduleController)
+                .build()
+    }
+
+    @Test
+    @Ignore
+    fun testSaveSchedule_returnsHttpStatusIsCreated() {
+
+        val schedule = Schedule("Name", "Description", ArrayList(), 1)
+
+        given(service.create(schedule)).willReturn(schedule)
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(schedule)))
+                .andExpect(status().isCreated)
+                .andExpect { content().equals(schedule) }
+    }
+
+    @Test
+    @Ignore
+    fun testSaveScheduleEmptyName_returnsHttpStatusIsBadRequest() {
+
+        val schedule = Schedule("", "Description", ArrayList(), 1)
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(schedule)))
+                .andExpect(status().isBadRequest)
+                .andExpect { jsonPath("message", equalTo("Object sent is not valid: [Schedule name cannot be empty!]")) }
+    }
+
+    @Test
+    @Ignore
+    fun testSaveScheduleEmptyDescription_returnsHttpStatusIsCreated() {
+
+        val schedule = Schedule("Name", "", ArrayList(), 1)
+
+        given(service.create(schedule)).willReturn(schedule)
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(schedule)))
+                .andExpect(status().isCreated)
+                .andExpect { content().equals(schedule) }
+    }
+
+    @Test
+    @Ignore
+    fun testSaveScheduleEmptyAmountOfTrainings_returnsHttpStatusIsBadRequest() {
+
+        val schedule = Schedule("Name", "Description", ArrayList(), 0)
+
+        given(service.create(schedule)).willReturn(schedule)
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(schedule)))
+                .andExpect(status().isBadRequest)
+                .andExpect { jsonPath("message", equalTo("Object sent is not valid: [Schedule needs at least one training per week!]")) }
+    }
+
+    @Test
+    @Ignore
+    fun testGetScheduleById_returnsHttpStatusOk() {
+
+        val schedule = Schedule("Name", "Description", ArrayList(), 0)
+        val id = 1L
+        given(service.findOne(id)).willReturn(schedule)
+
+        mockMvc.perform(
+                get(URI + "{id}", id))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect { content().equals(schedule) }
+
+
+    }
+
+    @Test
+    fun testGetScheduleById_returnsHttpStatusIsNotFound() {
+
+        val id = 1L
+        given(service.findOne(id)).willThrow(ResourceNotFoundException::class.java)
+
+        mockMvc.perform(
+                get(URI + "{id}", id))
+                .andExpect(status().isNotFound)
+    }
+
+    @Test
+    @Ignore
+    fun testGetAllSchedules_returnsHttpStatusOk() {
+
+        val results = arrayListOf(Schedule("Name", "Description", ArrayList(), 0),
+                Schedule("Name2", "Description2", ArrayList(), 0))
+
+
+        given(service.findAll()).willReturn(results)
+
+        mockMvc.perform(
+                get(URI))
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect { content().equals(results) }
+
+    }
+
+    @Test
+    @Ignore
+    fun testUpdateSchedule_returnsHttpStatusOk() {
+        val schedule = Schedule("Name", "Description", ArrayList(), 1)
+
+        given(service.update(schedule, 1L))
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(schedule)))
+                .andExpect(status().isOk)
+                .andExpect { content().equals(schedule) }
+
+    }
+
+    @Test
+    @Ignore
+    fun testUpdateScheduleInvalidForm_returnsHttpStatusBadRequest() {
+
+        val schedule = Schedule("", "Description", ArrayList(), 1)
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(schedule)))
+                .andExpect(status().isBadRequest)
+                .andExpect { jsonPath("message", equalTo("Object sent is not valid: [Schedule name cannot be empty!]")) }
+
+    }
+
+    @Test
+    fun testUpdateScheduleNonExistingSchedule_returnsHttpStatusNotFound() {
+
+        val schedule = Schedule("Name", "Description", ArrayList(), 1)
+
+        given(service.update(schedule, 1L)).willThrow(ResourceNotFoundException::class.java)
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(schedule)))
+                .andExpect(status().isNotFound)
+
+    }
+
+    @Test
+    @Ignore
+    fun testDeleteSchedule_returnsHttpStatusNoContent() {
+
+        val id = 1L
+
+        willDoNothing().given(service).delete(id)
+
+        mockMvc.perform(
+                delete(URI + "{id}", id))
+                .andExpect(status().isNoContent)
+                .andExpect(status().isNoContent)
+    }
+
+    private fun asJsonString(obj: Any): String = ObjectMapper().writeValueAsString(obj)
 
 }
--- src/test/kotlin/com/tsse/controller/UserControllerTests.kt
+++ src/test/kotlin/com/tsse/controller/UserControllerTests.kt
@@ -1,7 +1,25 @@
 package com.tsse.controller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.tsse.domain.model.User
+import com.tsse.service.UserService
+import org.hamcrest.Matchers
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.BDDMockito
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.springframework.data.rest.webmvc.ResourceNotFoundException
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import java.util.*
 
 /**
  *
@@ -12,169 +30,171 @@ import org.springframework.test.context.junit4.SpringRunner
 @RunWith(SpringRunner::class)
 class UserControllerTests {
 
-//    @Mock lateinit var service: UserService
-//
-//    @InjectMocks lateinit var userController: UserController
-//
-//    lateinit var mockMvc: MockMvc
-//
-//    val URI = "/api/users/"
-//
-//    @Before
-//    fun setup() {
-//        MockitoAnnotations.initMocks(UserControllerTests::class)
-//        mockMvc = MockMvcBuilders
-//                .standaloneSetup(userController)
-//                .build()
-//    }
-//
-//    @Test
-//    fun testSaveUsers_returnsHttpStatusIsCreated() {
-//
-//        val user = User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
-//
-//        BDDMockito.given(service.saveUser(user)).willReturn(user)
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.post(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(user)))
-//                .andExpect(MockMvcResultMatchers.status().isCreated)
-//                .andExpect { MockMvcResultMatchers.content().equals(user) }
-//    }
-//
-//    @Test
-//    @Ignore
-//    fun testSaveUserEmptyUsername_returnsHttpStatusIsBadRequest() {
-//
-//        val user = User("", "Password", true, Date(), "displayname", "firstname", "lastname", "status", "USER")
-//        println(user.birthdate)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.post(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(user)))
-//                .andExpect(MockMvcResultMatchers.status().isBadRequest)
-//                .andExpect { MockMvcResultMatchers.jsonPath("message", Matchers.equalTo("Object sent is not valid: [Username cannot be empty!]")) }
-//    }
-//
-//    @Test
-//    @Ignore
-//    fun testSaveUserEmptyPassword_returnsHttpStatusIsBadRequest() {
-//
-//        val user = User("x@y.z", "", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.post(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(user)))
-//                .andExpect(MockMvcResultMatchers.status().isBadRequest)
-//                .andExpect { MockMvcResultMatchers.jsonPath("message", Matchers.equalTo("Object sent is not valid: [Password cannot be empty!]")) }
-//    }
-//
-//    @Test
-//    fun testGetUserById_returnsHttpStatusOk() {
-//
-//        val user = User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
-//        val id = 1L
-//        BDDMockito.given(service.getUser(id)).willReturn(user)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.get(URI + "{id}", id))
-//                .andExpect(MockMvcResultMatchers.status().isOk)
-//                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
-//                .andExpect { MockMvcResultMatchers.content().equals(user) }
-//
-//
-//    }
-//
-//    @Test
-//    fun testGetUserById_returnsHttpStatusIsNotFound() {
-//
-//        val id = 1L
-//        BDDMockito.given(service.getUser(id)).willThrow(UserNotFoundException::class.java)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.get(URI + "{id}", id))
-//                .andExpect(MockMvcResultMatchers.status().isNotFound)
-//    }
-//
-//    @Test
-//    fun testGetAllUsers_returnsHttpStatusOk() {
-//
-//        val results = arrayListOf(User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER"),
-//                User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER"))
-//
-//
-//        BDDMockito.given(service.getAllUsers()).willReturn(results)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.get(URI))
-//                .andExpect(MockMvcResultMatchers.status().isOk)
-//                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
-//                .andExpect { MockMvcResultMatchers.content().equals(results) }
-//
-//    }
-//
-//    @Test
-//    fun testUpdateUser_returnsHttpStatusOk() {
-//        val user = User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
-//
-//        BDDMockito.given(service.updateUser(user)).willReturn(user)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.put(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(user)))
-//                .andExpect(MockMvcResultMatchers.status().isOk)
-//                .andExpect { MockMvcResultMatchers.content().equals(user) }
-//
-//    }
-//
-//
-//    @Test
-//    @Ignore
-//    fun testUpdateUserInvalidForm_returnsHttpStatusBadRequest() {
-//
-//        val user = User("x@y.z", "", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.put(URI)
-//                        .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                        .content(asJsonString(user)))
-//                .andExpect(MockMvcResultMatchers.status().isBadRequest)
-//                .andExpect { MockMvcResultMatchers.jsonPath("message", Matchers.equalTo("Object sent is not valid: [Username cannot be empty!]")) }
-//
-//    }
-//
-//    @Test
-//    fun testUpdateUserNonExistingUser_returnsHttpStatusNotFound() {
-//
-//        val user = User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
-//
-//        BDDMockito.given(service.updateUser(user)).willThrow(UserNotFoundException::class.java)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.put(URI)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(asJsonString(user)))
-//                .andExpect(MockMvcResultMatchers.status().isNotFound)
-//
-//    }
-//
-//    @Test
-//    fun testDeleteUser_returnsHttpStatusNoContent() {
-//
-//        val id = 1L
-//
-//        BDDMockito.willDoNothing().given(service).deleteUser(id)
-//
-//        mockMvc.perform(
-//                MockMvcRequestBuilders.delete(URI + "{id}", id))
-//                .andExpect(MockMvcResultMatchers.status().isNoContent)
-//
-//    }
-//
-//    private fun asJsonString(obj: Any): String = ObjectMapper().writeValueAsString(obj)
-//
+    @Mock lateinit var service: UserService
+
+    @InjectMocks lateinit var userController: UserController
+
+    lateinit var mockMvc: MockMvc
+
+    val URI = "/api/users/"
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(UserControllerTests::class)
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(userController)
+                .build()
+    }
+
+    @Test
+    fun testSaveUsers_returnsHttpStatusIsCreated() {
+
+        val user = User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
+
+        BDDMockito.given(service.create(user)).willReturn(user)
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(user)))
+                .andExpect(MockMvcResultMatchers.status().isCreated)
+                .andExpect { MockMvcResultMatchers.content().equals(user) }
+    }
+
+    @Test
+    @Ignore
+    fun testSaveUserEmptyUsername_returnsHttpStatusIsBadRequest() {
+
+        val user = User("", "Password", true, Date(), "displayname", "firstname", "lastname", "status", "USER")
+        println(user.birthdate)
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(user)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest)
+                .andExpect { MockMvcResultMatchers.jsonPath("message", Matchers.equalTo("Object sent is not valid: [Username cannot be empty!]")) }
+    }
+
+    @Test
+    @Ignore
+    fun testSaveUserEmptyPassword_returnsHttpStatusIsBadRequest() {
+
+        val user = User("x@y.z", "", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(user)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest)
+                .andExpect { MockMvcResultMatchers.jsonPath("message", Matchers.equalTo("Object sent is not valid: [Password cannot be empty!]")) }
+    }
+
+    @Test
+    fun testGetUserById_returnsHttpStatusOk() {
+
+        val user = User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
+        val id = 1L
+        BDDMockito.given(service.findOne(id)).willReturn(user)
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(URI + "{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect { MockMvcResultMatchers.content().equals(user) }
+
+
+    }
+
+    @Test
+    fun testGetUserById_returnsHttpStatusIsNotFound() {
+
+        val id = 1L
+        BDDMockito.given(service.findOne(id)).willThrow(ResourceNotFoundException::class.java)
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(URI + "{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isNotFound)
+    }
+
+    @Test
+    @Ignore
+    fun testGetAllUsers_returnsHttpStatusOk() {
+
+        val results = arrayListOf(User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER"),
+                User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER"))
+
+
+        BDDMockito.given(service.findAll()).willReturn(results)
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(URI))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect { MockMvcResultMatchers.content().equals(results) }
+
+    }
+
+    @Test
+    @Ignore
+    fun testUpdateUser_returnsHttpStatusOk() {
+        val user = User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
+
+        BDDMockito.given(service.update(user, 1L))
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(user)))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect { MockMvcResultMatchers.content().equals(user) }
+
+    }
+
+
+    @Test
+    @Ignore
+    fun testUpdateUserInvalidForm_returnsHttpStatusBadRequest() {
+
+        val user = User("x@y.z", "", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(URI)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(asJsonString(user)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest)
+                .andExpect { MockMvcResultMatchers.jsonPath("message", Matchers.equalTo("Object sent is not valid: [Username cannot be empty!]")) }
+
+    }
+
+    @Test
+    @Ignore
+    fun testUpdateUserNonExistingUser_returnsHttpStatusNotFound() {
+
+        val user = User("x@y.z", "Password", true, Date(), "displayName", "firstName", "lastName", "status", "USER")
+
+        BDDMockito.given(service.update(user, 1L)).willThrow(ResourceNotFoundException::class.java)
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.put(URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(user)))
+                .andExpect(MockMvcResultMatchers.status().isNotFound)
+
+    }
+
+    @Test
+    fun testDeleteUser_returnsHttpStatusNoContent() {
+
+        val id = 1L
+
+        BDDMockito.willDoNothing().given(service).delete(id)
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.delete(URI + "{id}", id))
+                .andExpect(MockMvcResultMatchers.status().isNoContent)
+
+    }
+
+    private fun asJsonString(obj: Any): String = ObjectMapper().writeValueAsString(obj)
 
 }
